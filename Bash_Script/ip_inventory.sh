721937adedfc7567a034751bc66b5e992b64edd4
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-06-08 01:33:07 +0300

    Update README.md

--- Bash_Script/ip_inventory.sh
+++
@@ -1,6 +0,0 @@
-cat <<EOF > ../Ansible/inventory
-[jenkins]
-jenkins.com ansible_host=$1 ansible_user=ubuntu ansible_ssh_private_key_file=~/Downloads/soanr.pem
-
-
-EOF
--- Terraform/Modules/network/eip.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_eip" "eip" {
-  
-  tags = {
-    Name = var.Name
-  }
-}
