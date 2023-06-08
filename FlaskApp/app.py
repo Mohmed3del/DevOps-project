fe58321afa4953c98593d5d985d1257fe6b72449
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-06-08 19:07:43 +0300

    Update source code

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -20,7 +20,6 @@ mysql.init_app(app)
 # set a secret key for the session
 app.secret_key = 'why would I tell you my secret key?'
 
-
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -30,8 +29,7 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
-
-@app.route('/signUp', methods=['POST', 'GET'])
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
     # read the posted values from the UI
     _name = request.form['inputName']
@@ -43,27 +41,26 @@ def signUp():
         conn = mysql.connect()
         cursor = conn.cursor()
 
-        # _hash_password = generate_password_hash(_password)
+        #_hash_password = generate_password_hash(_password) 
 
-        cursor.callproc('sp_createUser', (_name, _email, _password))
+        cursor.callproc('sp_createUser',(_name,_email,_password))
 
         data = cursor.fetchall()
 
         if len(data) == 0:
             conn.commit()
-            return json.dumps({'message': 'User created successfully !'})
+            return json.dumps({'message':'User created successfully !'})
         else:
-            return json.dumps({'error': str(data[0])})
+            return json.dumps({'error':str(data[0])})  
     else:
-        return json.dumps({'html': '<span>Enter the required fields</span>'})
+        return json.dumps({'html':'<span>Enter the required fields</span>'})     
 
 
 @app.route('/showSignIn')
 def showSignin():
     return render_template('signin.html')
 
-
-@app.route('/validateLogin', methods=['POST'])
+@app.route('/validateLogin',methods=['POST'])
 def validateLogin():
     try:
         _username = request.form['inputEmail']
@@ -71,97 +68,91 @@ def validateLogin():
 
         con = mysql.connect()
         cursor = con.cursor()
-        cursor.callproc('sp_validateLogin', (_username,))
+        cursor.callproc('sp_validateLogin',(_username,))
         data = cursor.fetchall()
         if len(data) > 0:
-            if data[0][3] == _password:
+            if data[0][3]==_password:
                 session['user'] = data[0][0]
                 return redirect('/userHome')
             else:
-                return render_template('error.html', error='Wrong Email address or Password')
+                return render_template('error.html',error = 'Wrong Email address or Password')
         else:
-            return render_template('error.html', error='Wrong Email address or Password')
+            return render_template('error.html',error='Wrong Email address or Password')
 
     except Exception as e:
-        return render_template('error.html', error=str(e))
+        return render_template('error.html',error=str(e))
     finally:
         cursor.close()
         con.close()
-
 
 @app.route('/userHome')
 def userHome():
     if session.get('user'):
         return render_template('userHome.html')
     else:
-        return render_template('error.html', error='Unauthorized Access')
-
+        return render_template('error.html',error = 'Unauthorized Access')
 
 @app.route('/logout')
 def logout():
-    session.pop('user', None)
+    session.pop('user',None)
     return redirect('/')
-
 
 @app.route('/showAddWish')
 def showAddWish():
     return render_template('addWish.html')
 
-
-@app.route('/addWish', methods=['POST'])
+@app.route('/addWish',methods=['POST'])
 def addWish():
     try:
         if session.get('user'):
             _title = request.form['inputTitle']
             _description = request.form['inputDescription']
             _user = session.get('user')
-
+ 
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.callproc('sp_addWish', (_title, _description, _user))
+            cursor.callproc('sp_addWish',(_title,_description,_user))
             data = cursor.fetchall()
-
+ 
             if len(data) == 0:
                 conn.commit()
                 return redirect('/userHome')
             else:
-                return render_template('error.html', error='An error occurred!')
-
+                return render_template('error.html',error = 'An error occurred!')
+ 
         else:
-            return render_template('error.html', error='Unauthorized Access')
+            return render_template('error.html',error = 'Unauthorized Access')
     except Exception as e:
-        return render_template('error.html', error=str(e))
+        return render_template('error.html',error = str(e))
     finally:
         cursor.close()
         conn.close()
-
 
 @app.route('/getWish')
 def getWish():
     try:
         if session.get('user'):
             _user = session.get('user')
-
+ 
             con = mysql.connect()
             cursor = con.cursor()
-            cursor.callproc('sp_GetWishByUser', (_user,))
+            cursor.callproc('sp_GetWishByUser',(_user,))
             wishes = cursor.fetchall()
-
+ 
             wishes_dict = []
             for wish in wishes:
                 wish_dict = {
-                    'Id': wish[0],
-                    'Title': wish[1],
-                    'Description': wish[2],
-                    'Date': wish[4]}
+                        'Id': wish[0],
+                        'Title': wish[1],
+                        'Description': wish[2],
+                        'Date': wish[4]}
                 wishes_dict.append(wish_dict)
-
+ 
             return json.dumps(wishes_dict)
         else:
-            return render_template('error.html', error='Unauthorized Access')
+            return render_template('error.html', error = 'Unauthorized Access')
     except Exception as e:
-        return render_template('error.html', error=str(e))
-
+        return render_template('error.html', error = str(e))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0",port=5002,debug=True)
--- MySQL_Queries/BucketList.sql
+++ MySQL_Queries/BucketList.sql
@@ -1,38 +1,38 @@
 -- Local database definition.
 CREATE DATABASE BucketList;
 CREATE TABLE `BucketList`.`tbl_user` (
-  `user_id` BIGINT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NULL,
-  `user_username` VARCHAR(45) NULL,
-  `user_password` VARCHAR(45) NULL,
-  PRIMARY KEY (`user_id`)
+    `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_name` VARCHAR(45) NULL,
+    `user_username` VARCHAR(45) NULL,
+    `user_password` VARCHAR(45) NULL,
+    PRIMARY KEY (`user_id`)
 );
 USE BucketList;
 INSERT INTO tbl_user
-values (10, 'mohmed', 'mohmed', 'mohmed');
+values (10, 'Mohmed', 'Mohmed@gmail.com', 'Mohmed');
 DELIMITER $$ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_createUser`(
-  IN p_name VARCHAR(20),
-  IN p_username VARCHAR(100),
-  IN p_password VARCHAR(20)
+    IN p_name VARCHAR(20),
+    IN p_username VARCHAR(100),
+    IN p_password VARCHAR(20)
 ) BEGIN if (
-  select exists (
-      select 1
-      from tbl_user
-      where user_username = p_username
-    )
+    select exists (
+            select 1
+            from tbl_user
+            where user_username = p_username
+        )
 ) THEN
 select 'Username Exists !!';
 ELSE
 insert into tbl_user (
-    user_name,
-    user_username,
-    user_password
-  )
+        user_name,
+        user_username,
+        user_password
+    )
 values (
-    p_name,
-    p_username,
-    p_password
-  );
+        p_name,
+        p_username,
+        p_password
+    );
 END IF;
 END $$ DELIMITER;
 DELIMITER $$ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_validateLogin`(IN p_username VARCHAR(20)) BEGIN
@@ -41,32 +41,32 @@ from tbl_user
 where user_username = p_username;
 END $$ DELIMITER;
 CREATE TABLE `BucketList`.`tbl_wish` (
-  `wish_id` int(11) NOT NULL AUTO_INCREMENT,
-  `wish_title` varchar(45) DEFAULT NULL,
-  `wish_description` varchar(5000) DEFAULT NULL,
-  `wish_user_id` int(11) DEFAULT NULL,
-  `wish_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`wish_id`)
+    `wish_id` int(11) NOT NULL AUTO_INCREMENT,
+    `wish_title` varchar(45) DEFAULT NULL,
+    `wish_description` varchar(5000) DEFAULT NULL,
+    `wish_user_id` int(11) DEFAULT NULL,
+    `wish_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`wish_id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = latin1;
 USE `BucketList`;
 DROP procedure IF EXISTS `BucketList`.`sp_addWish`;
 DELIMITER $$ USE `BucketList` $$ CREATE DEFINER = `root` @`localhost` PROCEDURE `sp_addWish`(
-  IN p_title varchar(45),
-  IN p_description varchar(1000),
-  IN p_user_id bigint
+    IN p_title varchar(45),
+    IN p_description varchar(1000),
+    IN p_user_id bigint
 ) BEGIN
 insert into tbl_wish(
-    wish_title,
-    wish_description,
-    wish_user_id,
-    wish_date
-  )
+        wish_title,
+        wish_description,
+        wish_user_id,
+        wish_date
+    )
 values (
-    p_title,
-    p_description,
-    p_user_id,
-    NOW()
-  );
+        p_title,
+        p_description,
+        p_user_id,
+        NOW()
+    );
 END $$ DELIMITER;
 ;
 USE `BucketList`;
