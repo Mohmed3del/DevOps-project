2f235b21e9fa2389065c7acdbef3f8384dd4b9a9
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-08-01 01:08:06 +0300

    add bash script to create docker-secret for download images from private repo

---
+++ create _secret.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define variables
+ECR_REGION=us-east-1
+ECR_ACCOUNT_ID=889149267524
+SECRET_NAME=ecr-secret
+# EMAIL=<email>
+
+# Retrieve ECR authentication token
+TOKEN=$(aws ecr get-login-password --region $ECR_REGION)
+
+# Create Kubernetes secret
+kubectl create secret docker-registry $SECRET_NAME \
+  --docker-server=$ECR_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$TOKEN 
+#   --docker-email=$EMAIL
+
