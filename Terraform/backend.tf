61b7f154ee445abe0dac93357782a30080ebfd82
Mohmed3del <mohmed.adel.188.2017@gmail.com>
2023-08-31 11:54:22 +0300

    update backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "te1st"
-    key            = "devops/terraform.tfstate"
+    key            = "DevOps/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "backend"
   }
